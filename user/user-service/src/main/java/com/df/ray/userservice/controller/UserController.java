a95c9520ec4dcfbeccff08221c87fbf723459469
toxicray <623888433@qq.com>
2021-11-18 22:49:30 +0800

    接口尝试

---
+++ user/user-service/src/main/java/com/df/ray/userservice/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.df.ray.userservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * UserController
+ *
+ * @author ray
+ * @date 2021/11/17
+ */
+@RestController
+public class UserController {
+}
