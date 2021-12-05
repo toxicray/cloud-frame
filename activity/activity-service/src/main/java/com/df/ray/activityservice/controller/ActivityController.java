9da1d0f799cb340e37d4c32592b26f064799a5df
toxicray <623888433@qq.com>
2021-12-05 20:11:33 +0800

    服务间添加熔断控制

--- activity/activity-service/src/main/java/com/df/ray/activityservice/controller/ActivityController.java
+++ activity/activity-service/src/main/java/com/df/ray/activityservice/controller/ActivityController.java
@@ -2,6 +2,8 @@ package com.df.ray.activityservice.controller;
 
 import com.df.ray.model.UserInfo;
 import com.df.ray.userapi.IFeignUserService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +26,24 @@ public class ActivityController {
     }
 
     @GetMapping("user")
+//    @HystrixProperty(value = "3000", name = "metrics.rollingStats.timeInMilliseconds"),
+//    @HystrixProperty(value = "2", name = "circuitBreaker.requestVolumeThreshold"),
+//    @HystrixProperty(value = "50", name = "circuitBreaker.errorThresholdPercentage"),
+//    @HystrixProperty(value = "5", name = "circuitBreaker.sleepWindowInMilliseconds"),
+    @HystrixCommand(
+            commandProperties = {
+                    @HystrixProperty(value = "2000", name = "execution.isolation.thread.timeoutInMilliseconds")
+            },
+            fallbackMethod = "firstFallBack"
+    )
     public UserInfo getActivityUser() {
         return iFeignUserService.getUserInfo();
+    }
+
+    private UserInfo firstFallBack() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setName("张三");
+        userInfo.setValue("嘿哈");
+        return userInfo;
     }
 }
